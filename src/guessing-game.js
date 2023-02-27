a9a69f6646e1d4d1383d778210a394c1b19cf958
DrReverden <lamafon@mail.ru>
2023-02-27 14:51:17 +0300

    add: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.centerNumber = Math.ceil((this.min + this.max) / 2)
     }
 
     lower() {
-
+        return this.max = this.centerNumber;
     }
 
     greater() {
-
+        return this.min = this.centerNumber;
     }
 }
 
